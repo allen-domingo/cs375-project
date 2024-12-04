fc4367e174b77ac8e4c9d5fa756694396120a10e
Allen Domingo <adomin34@binghamton.edu>
2024-12-03 19:32:15 -0500

    from center revised

--- from_center.cpp
+++ from_center.cpp
@@ -1,33 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-
+#include <algorithm>
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 
 
-int expand_from_center(string &s){
-  int length = s.length();
+string expand_from_center(string &s1, string &s2){
+  int length1 = s1.length(), length2 = s2.length();
+  string rev_s2 = s2;
+  reverse(rev_s2.begin(), rev_s2.end());
+   string longestPal = "";
 
-  int start = 0, max = 1;
+  int start = 0, max = 0;
+ 
+  for(int i = 0; i < length1; i++){
 
-  for(int i = 0; i < length; i++){
-    for(int j = 0; j <=1; j++{
+    for(int j = 0; j <=1; j++){
+
+     
 	int l = i;
 	int h = i + j;
-	while(l >= 0 && h < length && s[l] == s[h]){
-	  int curr = h- l + 1;
-	  if(curr > max){
+	while(l >= 0 && h < length1 && s1[l] == s1[h]){
+	  int curr = h-l+1;
+	  string candidate = s1.substr(l,h-l +1);
+	  if(rev_s2.find(candidate) != string::npos &&  curr > max){
 	    start = l;
 	    max = curr;
+	    longestPal = candidate;
+
 	  }
+   
 	  l--;
 	  h++;
 	}
-	
-      }
-
+    }
   }
-    //print sub from start to max indexes
-  return 0; 
+  if(longestPal.size() == 0){
+    return  "";
+  }
+    return longestPal; 
 }
+
+int main(){
+  ifstream file("tests.txt");
+  string s1 = "abcdef";
+  string s2 = "gfedcba";
+
+  cout << "exp from center longest:" <<  expand_from_center(s1,s2) << endl;
+  return 0;
+    
+}
+
+ 
